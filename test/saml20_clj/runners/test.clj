c86182ae9e998cb78d1c9c8a2c52dbf02811ede2
Cam Saul <github@camsaul.com>
2022-02-15 13:44:54 -0800

    Add :refer-clojure :exclude

--- test/saml20_clj/runners/test.clj
+++ test/saml20_clj/runners/test.clj
@@ -1,4 +1,5 @@
 (ns saml20-clj.runners.test
+  (:refer-clojure :exclude [test])
   (:require [cognitect.test-runner.api :as test-runner]
             [pjstadig.humane-test-output :as humane-test-output]))
 
